cd78c7066fb200dbd4a162e63479092365a5e40c
lgferreyra <leonardogferreyra@gmail.com>
2016-04-23 21:22:27 -0300

    Leonardo
    Se agrego el script firebase y el html de baja y modificacion

--- alumnoalta/www/js/baja.js
+++ alumnoalta/www/js/baja.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers')
 	.controller("BajaCtrl", function($scope, $stateParams){
 		$scope.titulo = "Eliminar Alumno";
+		$scope.icono = "ion-close";
+		$scope.boton = "button-assertive";
 	});
 
 	
--- alumnoalta/www/js/modificacion.js
+++ alumnoalta/www/js/modificacion.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers')
 	.controller("ModificacionCtrl", function($scope, $stateParams){
 		$scope.titulo = "Modificar Alumno";
+		$scope.icono = "ion-edit";
+		$scope.boton = "button-calm";
 	});
 
 	
